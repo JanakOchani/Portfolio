2ef351688d638631fb6a845aa288cf1c6bf55232
JanakOchani <janakochani@gmail.com>
2026-02-01 17:16:18 -0500

    Portfolio Project

--- PortfolioMain.py
+++ PortfolioMain.py
@@ -1,6 +1,6 @@
 from PortfolioUI import PortfolioUI
 
-#main program to run. This is what we can run
+#main program to run. This is what we can run, Letssee
 
 #call Portfolio UI class to display Menu Option and take it from there.
 portfolioUI = PortfolioUI()
