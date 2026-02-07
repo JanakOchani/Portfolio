5599efaa27ed1301aa83d60dff9ba970f813639b
Janak Ochani <janakochani@gmail.com>
2026-02-07 16:08:46 -0500

    commit changes like  db password

---
+++ testclass.py
@@ -0,0 +1,15 @@
+class Janak:
+
+    def __init__(self, name, country):
+        self.name = name
+        self.country = country
+
+    def greet_user(self):
+        print(f"Greetings",self.name,"!")
+        self.__what_country()
+
+    def __what_country(self):
+        print(" My age is ",self.country)
+
+
+
